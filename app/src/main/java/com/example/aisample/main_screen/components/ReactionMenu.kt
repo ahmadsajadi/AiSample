d78dca2e722d444ac7497d4d3fa59a5b1718e1e6
fanap-dotin <sa.sajjadi@dotin.ir>
2024-09-28 17:54:14 +0330

    clean

--- app/src/main/java/com/example/aisample/main_screen/components/ReactionMenu.kt
+++ app/src/main/java/com/example/aisample/main_screen/components/ReactionMenu.kt
@@ -1,10 +1,8 @@
 package com.example.aisample.main_screen.components
 
-import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
